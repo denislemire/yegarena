f8dbab2055cb7e6041dce9de4963531ccaa30d42
Denis Lemire <denis@lemire.name>
2014-09-10 13:03:17 -0600

    Clean-up the snap.sh script...

--- snap.sh
+++ snap.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
-if [ ! -d /tmp/yegarena ]; then
-	/bin/mkdir /tmp/yegarena
+EADTMPSTASH='/tmp/yegarena'
+EADDIR='/home/denis/yegarena'
+EADHOST='frosty.incoherency.net'
+EADHOSTPATH='public_html/yegarena'
+
+# Create the working dir in tmp
+if [ ! -d $EADTMPSTASH ]; then
+	/bin/mkdir $EADTMPSTASH
 fi
 
-/usr/bin/fswebcam -c /home/denis/yegarena/fswebcam.conf
+# Papa-paparazzi
+/usr/bin/fswebcam -c $EADDIR/fswebcam.conf
 
-for file in /tmp/yegarena/output-*; do scp "$file" frosty.incoherency.net:public_html/yegarena && rm "$file" ; done
+# Copy the images and remove locals on success
+for file in $EADTMPSTASH/output-*; do scp "$file" $EADHOST:$EADHOSTPATH && rm "$file" ; done
 
 # Append the remote log file
-cat /tmp/yegarena/webcamlog.txt | ssh frosty.incoherency.net "cat >> /home/denis/public_html/yegarena/webcamlog.txt" && rm /tmp/yegarena/webcamlog.txt
+cat $EADTMPSTASH/webcamlog.txt | ssh $EADHOST "cat >> $EADHOSTPATH/webcamlog.txt" && rm $EADTMPSTASH/webcamlog.txt
 
-/usr/bin/ssh frosty.incoherency.net public_html/yegarena/link.sh
+/usr/bin/ssh $EADHOST $EADHOSTPATH/link.sh
