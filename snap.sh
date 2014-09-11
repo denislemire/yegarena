fcecdc136f1d08438fb881a2b1542ad91b73e518
Denis Lemire <denis@lemire.name>
2014-09-11 13:12:16 -0600

    More self contained, no need for link.sh on remote host.

--- snap.sh
+++ snap.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+#
+# Script to gather images for the EAD time-lapse
+#
+# http://www.denis.lemire.name/yegarena/
+#
+# Denis Lemire, 2014
+#
+
 EADTMPSTASH='/tmp/yegarena'
 EADDIR='/home/denis/yegarena'
 EADHOST='frosty.incoherency.net'
@@ -14,9 +22,20 @@ fi
 /usr/bin/fswebcam -c $EADDIR/fswebcam.conf
 
 # Copy the images and remove locals on success
-for file in $EADTMPSTASH/output-*; do scp "$file" $EADHOST:$EADHOSTPATH && rm "$file" ; done
+for file in $(/bin/ls -1t $EADTMPSTASH/output-*)
+do
+	scp -p "$file" $EADHOST:$EADHOSTPATH
+
+	if [[ $? == 0 ]]; then
+		/bin/rm $file
+		LATEST=`/usr/bin/basename $file`
+	else
+		exit
+	fi
+done
+
+# Symlink the latest image
+/usr/bin/ssh $EADHOST "rm $EADHOSTPATH/output-latest.jpg && ln -s $LATEST $EADHOSTPATH/output-latest.jpg"
 
 # Append the remote log file
-cat $EADTMPSTASH/webcamlog.txt | ssh $EADHOST "cat >> $EADHOSTPATH/webcamlog.txt" && rm $EADTMPSTASH/webcamlog.txt
-
-/usr/bin/ssh $EADHOST $EADHOSTPATH/link.sh
+/bin/cat $EADTMPSTASH/webcamlog.txt | /usr/bin/ssh $EADHOST "cat >> $EADHOSTPATH/webcamlog.txt" && rm $EADTMPSTASH/webcamlog.txt
